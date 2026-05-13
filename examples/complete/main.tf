5c36982e2eb5c3d37ad1955bb18e3004aa295037
Matheus Thurler <matheusthurlernf@gmail.com>
2026-05-12 23:42:42 -0300

    chore: fix formatting and add go.sum
    - Fix terraform fmt on examples/complete/main.tf
    - Add go.sum after go mod tidy (required for Terratest)

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -9,17 +9,17 @@ module "gke_spot" {
   network    = var.network
   subnetwork = var.subnetwork
 
-  node_count     = var.node_count
-  machine_type   = var.machine_type
-  spot_enabled   = var.spot_enabled
-  disk_size_gb   = var.disk_size_gb
-  disk_type      = var.disk_type
+  node_count   = var.node_count
+  machine_type = var.machine_type
+  spot_enabled = var.spot_enabled
+  disk_size_gb = var.disk_size_gb
+  disk_type    = var.disk_type
 
   min_node_count = var.min_node_count
   max_node_count = var.max_node_count
 
-  kubernetes_version    = var.kubernetes_version
-  enable_network_policy = var.enable_network_policy
+  kubernetes_version     = var.kubernetes_version
+  enable_network_policy  = var.enable_network_policy
   maintenance_start_time = var.maintenance_start_time
 
   labels = var.labels
